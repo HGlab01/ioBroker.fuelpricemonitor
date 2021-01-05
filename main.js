5d594ab33c7d6d5bc14435b92673d5e89cb39f09
HGlab <54232526+HGlab01@users.noreply.github.com>
2021-01-05 23:56:46 +0100

    improvement

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ class FuelPriceMonitor extends utils.Adapter {
         });
         this.on('ready', this.onReady.bind(this));
         //this.on('objectChange', this.onObjectChange.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
+        //this.on('stateChange', this.onStateChange.bind(this));
         //this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
         this.createdStatesDetails = {};
