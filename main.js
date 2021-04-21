52df0692024844bae5dc3693af136dcdf187d894
HGlab <54232526+HGlab01@users.noreply.github.com>
2021-04-21 20:22:32 +0200

    release v0.2.4
    * fix deleteEverything issue for Super95
    * add feature (experimental!) to sort by ID instead of price (helps to monitor one specific gas station)
    * add attributes 'from', 'to', 'day' and 'orders'
    * Bump iobroker-jsonexplorer to v0.0.0-18

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ const stateAttr = require(`${__dirname}/lib/stateAttr.js`); // Load attribute li
 let dieselSelected = false;
 let superSelected = false;
 let gasSelected = false;
+let useIDs = false;
 
 class FuelPriceMonitor extends utils.Adapter {
 
@@ -44,10 +45,12 @@ class FuelPriceMonitor extends utils.Adapter {
     async onReady() {
         // Initialize adapter
         //get adapter configuration
+        this.log.info('Started with JSON-Explorer version ' + JsonExplorer.version);
         dieselSelected = this.config.diesel;
         superSelected = this.config.super;
         gasSelected = this.config.gas;
         this.log.debug(`Diesel | Super | CNG for location Home selected : ${dieselSelected} | ${superSelected} | ${gasSelected}`);
+        if(this.config.useIDs) useIDs = this.config.useIDs;
 
         //subscribe relevant states changes
         //this.subscribeStates('STATENAME');
@@ -72,6 +75,7 @@ class FuelPriceMonitor extends utils.Adapter {
         this.log.debug('LONGITUDE from config: ' + this.longitude);
 
         let result = await this.ExecuteRequest();
+        
         if (result == 'error') {
             this.terminate ? this.terminate(utils.EXIT_CODES.INVALID_CONFIG_OBJECT) : process.exit(0);
         } else {
@@ -148,21 +152,21 @@ class FuelPriceMonitor extends utils.Adapter {
             if (dieselSelected) {
                 result = await this.getData('DIE', this.latitude, this.longitude);
                 this.log.debug(`JSON-Response for location Home Diesel: ${JSON.stringify(result)}`);
-                await JsonExplorer.TraverseJson(result, '0_Home_Diesel', true, false);
+                await JsonExplorer.TraverseJson(result, '0_Home_Diesel', true, useIDs);
             } else {
                 await JsonExplorer.deleteEverything('0_Home_Diesel');
             }
             if (superSelected) {
                 result = await this.getData('SUP', this.latitude, this.longitude);
                 this.log.debug(`JSON-Response for location Home Super: ${JSON.stringify(result)}`);
-                await JsonExplorer.TraverseJson(result, '0_Home_Super95', true, false);
+                await JsonExplorer.TraverseJson(result, '0_Home_Super95', true, useIDs);
             } else {
-                await JsonExplorer.deleteEverything('0_Home_Super');
+                await JsonExplorer.deleteEverything('0_Home_Super95');
             }
             if (gasSelected) {
                 result = await this.getData('GAS', this.latitude, this.longitude);
                 this.log.debug(`JSON-Response for location Home CNG: ${JSON.stringify(result)}`);
-                await JsonExplorer.TraverseJson(result, '0_Home_CNG', true, false);
+                await JsonExplorer.TraverseJson(result, '0_Home_CNG', true, useIDs);
             } else {
                 await JsonExplorer.deleteEverything('0_Home_CNG');
             }
@@ -205,7 +209,7 @@ class FuelPriceMonitor extends utils.Adapter {
                     case 'SUP': fuelType = 'Super95'; break;
                     case 'GAS': fuelType = 'CNG'; break;
                 }
-                await JsonExplorer.TraverseJson(result, `${location}_${fuelType}`, true, false);
+                await JsonExplorer.TraverseJson(result, `${location}_${fuelType}`, true, useIDs);
             }
 
             await JsonExplorer.checkExpire('*');
